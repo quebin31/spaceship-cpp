42c419ac266da17712af61b4c6c0a733bae18d8e
lans98 <lans9831@gmail.com>
2016-11-23 08:25:50 -0500

    nothing

--- main.cpp
+++ main.cpp
@@ -34,14 +34,13 @@ int main(int argc, const char **argv) {
     al_wait_for_event(SpaceShip.get_event_queue() ,&eve);
 
     if (eve.type == ALLEGRO_EVENT_KEY_UP){
-      if(eve.keyboard.keycode == ALLEGRO_KEY_ESCAPE){
+      if(eve.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
         done=true;
-      }
     }
-    else if(eve.type == ALLEGRO_EVENT_KEY_DOWN){
-      if(eve.keyboard.keycode == ALLEGRO_KEY_ENTER){
+    else if(eve.type == ALLEGRO_EVENT_KEY_DOWN) {
+      if (eve.keyboard.keycode == ALLEGRO_KEY_ENTER) {
 
-        SpaceShip.set_display_color(0,0,0);
+        SpaceShip.set_display_color(0, 0, 0);
         nave.draw_nave(NAVE_UP);
         myAsters.draw_asteroids();
         al_flip_display();
@@ -53,73 +52,45 @@ int main(int argc, const char **argv) {
 
 
           if (ev.type == ALLEGRO_EVENT_TIMER) {
-            if (keyboard.get_key_state(UP) && nave.getY() >= 4.0){
+            if (keyboard.get_key_state(UP) && nave.getY() >= 4.0) {
               nave.moveY(-4.0);
-              direction=NAVE_UP;
+              direction = NAVE_UP;
               SpaceShip.redraw = true;
-            }
-            else if (keyboard.get_key_state(DOWN) && nave.getY() <= screen->height - nave.getH() - 4.0){
+            } else if (keyboard.get_key_state(DOWN) && nave.getY() <= screen->height - nave.getH() - 4.0) {
               nave.moveY(4.0);
-              direction=NAVE_UP;
+              direction = NAVE_UP;
               SpaceShip.redraw = true;
-            }
-            else if (keyboard.get_key_state(LEFT) && nave.getX() >= 4.0){
+            } else if (keyboard.get_key_state(LEFT) && nave.getX() >= 4.0) {
               nave.moveX(-4.0);
-              direction=NAVE_LEFT;
+              direction = NAVE_LEFT;
               SpaceShip.redraw = true;
-            }
-            else if (keyboard.get_key_state(RIGHT) && nave.getX() <= screen->width - nave.getW() - 4.0) {
+            } else if (keyboard.get_key_state(RIGHT) && nave.getX() <= screen->width - nave.getW() - 4.0) {
               nave.moveX(4.0);
               direction = NAVE_RIGHT;
               SpaceShip.redraw = true;
             }
 
-            if (keyboard.get_key_state(CHAR_A)){
+            if (keyboard.get_key_state(CHAR_A)) {
               nave.shoot();
               keyboard.change_key_state(CHAR_A, false);
             }
 
-            SpaceShip.set_display_color(0,0,0);
+            SpaceShip.set_display_color(0, 0, 0);
             myAsters.move_asteroids();
             myAsters.draw_asteroids();
             nave.draw_nave(direction);
             al_flip_display();
             cout << SpaceShip.get_timer_count() << endl;
 
-          }
-          else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+          } else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
             break;
           else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
             keyboard.key_down_event(ev);
           else if (ev.type == ALLEGRO_EVENT_KEY_UP)
             keyboard.key_up_event(ev, SpaceShip);
-<<<<<<< HEAD
-          if (SpaceShip.redraw && SpaceShip.event_queue_is_empty()){
-=======
-
-          // fixme: ¿Es necesaria esta parte?
-          /*if (SpaceShip.redraw && SpaceShip.event_queue_is_empty()){
->>>>>>> 7b8fe6f5a7ebe56c105a7de2d6aa958800b5d20e
-            SpaceShip.redraw = false;
-            SpaceShip.set_display_color(0,0,0);
-            nave.draw_nave(direction);
-            al_flip_display();
-          }*/
         }
       }
     }
-    else if (eve.type==ALLEGRO_EVENT_DISPLAY_CLOSE)
-      done=true;
-<<<<<<< HEAD
-    // fixme: Realmente es necesario, mas arriba se dibuja la nave y los asteroides.
-//    if (SpaceShip.redraw && SpaceShip.event_queue_is_empty()){
-//      SpaceShip.redraw = false;
-//      SpaceShip.set_display_color(0,0,0);
-//      nave.draw_nave(direction);
-//      al_flip_display();
-//    }
-=======
->>>>>>> 7b8fe6f5a7ebe56c105a7de2d6aa958800b5d20e
   }
 
   cout << al_get_time() << endl;
