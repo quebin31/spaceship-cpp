e55e558ca10841d74e189abfbc00da82061d38b6
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2016-12-18 11:19:00 -0500

    .

---
+++ strategys/strategy_nave_gun.cpp
@@ -0,0 +1,6 @@
+#include "strategy_nave_gun.h"
+
+strategy_nave_gun::strategy_nave_gun()
+{
+  //ctor
+}
---
+++ strategys/strategy_nave_gun.h
@@ -0,0 +1,15 @@
+#ifndef STRATEGY_NAVE_GUN_H
+#define STRATEGY_NAVE_GUN_H
+
+
+class strategy_nave_gun
+{
+  public:
+    strategy_nave_gun();
+
+  protected:
+
+  private:
+};
+
+#endif // STRATEGY_NAVE_GUN_H
