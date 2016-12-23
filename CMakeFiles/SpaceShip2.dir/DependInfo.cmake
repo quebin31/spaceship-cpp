f5f70bfb19fe41ec449152139cc4e5a9f10baeee
lans98 <lans9831@gmail.com>
2016-12-22 19:20:20 -0500

    readme

---
+++ CMakeFiles/SpaceShip2.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/IPOO/SpaceShip2/bitmap_interfaces/asteroids_interface.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_interfaces/asteroids_interface.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_interfaces/bullet_interface.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_interfaces/bullet_interface.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_interfaces/game_facade.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_interfaces/game_facade.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_interfaces/hearts_interface.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_interfaces/hearts_interface.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_interfaces/power_up_interface.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_interfaces/power_up_interface.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/asteroid.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/asteroid.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/bitmap.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/bitmap.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/bullet.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/bullet.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/heart.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/heart.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/image.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/image.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/nave.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/nave.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/bitmap_objects/power_up.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/bitmap_objects/power_up.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/game_interfaces/keyboard.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/game_interfaces/keyboard.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/game_interfaces/main_game.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/game_interfaces/main_game.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/main.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/main.cpp.o"
+  "/home/kevin/IPOO/SpaceShip2/tools_and_headers/int_to_string.cpp" "/home/kevin/IPOO/SpaceShip2/CMakeFiles/SpaceShip2.dir/tools_and_headers/int_to_string.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/allegro5"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
